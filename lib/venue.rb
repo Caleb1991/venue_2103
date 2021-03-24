67d435c5d597cdc5f18f1c9c94ae01282c0a2ae1
Caleb Wittman <calebjwittman1991@gmail.com>
2021-03-24 15:41:10 -0600

    Created Venue class, added methods within, as well as creating tests in the venue_spec file

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,43 @@
 class Venue
+  attr_reader :name,
+              :capacity,
+              :patrons
+
+  def initialize(name, capacity, patrons = [ ])
+    @name     = name
+    @capacity = capacity
+    @patrons  = patrons
+  end
+
+  def add_patron(guest)
+    @patrons << guest
+  end
+
+
+  # In this method .upcase gives an error. In researching I found
+  # that .map(&:upcase) returned the desired result, however I
+  # cannot tell you what .map does so I will not be including it for
+  # this exercise. I am guessing the problem is that even though
+  # the contents of this array are strings, Ruby doesn't recognize
+  # that and will not complete the .upcase method unless some
+  # other method is passed through to tell it is ok.
+  def yell_at_patrons
+    @patrons.upcase
+  end
+
+  def over_capacity?
+    return true if @patrons.length >= 5
+    false
+  end
+
+  def kick_out
+    loop do
+      @patrons.pop
+
+      if @patrons.length == 4
+        break
+      end
+    end
+  end
+
 end
