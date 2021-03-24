72984d1c2416b82b588a2ca60f57c4d63f2eaf7d
Caleb Wittman <calebjwittman1991@gmail.com>
2021-03-24 15:48:51 -0600

    Updated comments above yell_at_patrons

--- lib/venue.rb
+++ lib/venue.rb
@@ -20,7 +20,9 @@ class Venue
   # this exercise. I am guessing the problem is that even though
   # the contents of this array are strings, Ruby doesn't recognize
   # that and will not complete the .upcase method unless some
-  # other method is passed through to tell it is ok.
+  # other method is passed through to tell it is ok. I was thinking
+  # it would be using #each but then I went down the rabbit hole of
+  # well then I'd need to shovel it into a new array and so on.
   def yell_at_patrons
     @patrons.upcase
   end
